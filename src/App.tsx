3cd368beff0c6c6204d9b293c65ee88c0c24a7f4
SuQuocphan <142078425+QuocPhan5659@users.noreply.github.com>
2026-04-20 14:10:01 +0700

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import ImageSplitter from './components/ImageSplitter';
-
-export default function App() {
-  return (
-    <div className="min-h-screen">
-      <ImageSplitter />
-    </div>
-  );
-}
